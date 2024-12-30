2b245b1250ae4657ab339e2d54f20465df30a65a
Eli Pacheco Hoyos <eph_074@hotmail.com>
2024-12-30 17:54:46 -0500

    Create project

---
+++ PlaceFinder/App/PlaceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceFinderApp.swift
+//  PlaceFinder
+//
+//  Created by Eli Pacheco Hoyos on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaceFinderTests/PlaceFinderTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceFinderTests.swift
+//  PlaceFinderTests
+//
+//  Created by Eli Pacheco Hoyos on 30/12/24.
+//
+
+import Testing
+
+struct PlaceFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
